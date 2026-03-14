e0c2c829a986f1d2fa409d7622266d2da374f7df
Ian <ianzinhobarrueco@gmail.com>
2026-03-13 22:56:39 -0300

    feat: segunda implementeção

--- cad-usuarios.php
+++ cad-usuarios.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cad-Usuários</title>
+        <link rel="stylesheet" href="index.css">
+
     <link rel="stylesheet" href="cad-usuarios.css">
 </head>
 <body>
+    <div class="dashboard-container">
 
 <?php include 'menu.php';?>
 
@@ -53,7 +56,7 @@
         </section>
 
     </main>
-
+</div>
 <?php include 'footer.php';?>
 
 </body>
